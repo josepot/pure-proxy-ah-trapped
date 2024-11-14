5149625ade20daf1a2c3b1b828fd10b49a1b6067
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-11-14 22:46:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# pure-proxy-ah-trapped
+
+It queries the pure-proxy accounts that exist on the Polkadot Relay-Chain which have sufficients on Polkadot Asset Hub.
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
---
+++ index.ts
@@ -0,0 +1,38 @@
+import { dot, dotAh } from "@polkadot-api/descriptors"
+import { createClient } from "polkadot-api"
+import { chainSpec } from "polkadot-api/chains/polkadot"
+import { chainSpec as chainSpecAh } from "polkadot-api/chains/polkadot_asset_hub"
+import { start } from "polkadot-api/smoldot"
+import { getSmProvider } from "polkadot-api/sm-provider"
+
+const smoldot = start({ logCallback: () => {} })
+const relayChain = smoldot.addChain({ chainSpec })
+const ahChain = relayChain.then((chain) =>
+  smoldot.addChain({
+    chainSpec: chainSpecAh,
+    potentialRelayChains: [chain],
+  }),
+)
+const [relayClient, ahClient] = [relayChain, ahChain].map((chain) =>
+  createClient(getSmProvider(chain)),
+)
+const dotApi = relayClient.getTypedApi(dot)
+const ahApi = ahClient.getTypedApi(dotAh)
+
+const potentialAddresses = (await dotApi.query.Proxy.Proxies.getEntries()).map(
+  ({ keyArgs: [address] }) => address,
+)
+const problematicAddresses = (
+  await ahApi.query.System.Account.getValues(potentialAddresses.map((x) => [x]))
+)
+  .map(({ sufficients }, idx) => ({
+    address: potentialAddresses[idx],
+    sufficients,
+  }))
+  .filter(({ sufficients }) => sufficients > 0)
+  .map(({ address }) => address)
+console.log(problematicAddresses)
+
+relayClient.destroy()
+ahClient.destroy()
+smoldot.terminate()
