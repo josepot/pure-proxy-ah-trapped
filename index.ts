2792a7f356cc5ca9b936b6ee6405c2146fe9fe3f
Josep M Sobrepere <jm.sobrepere@gmail.com>
2024-11-19 21:50:43 +0100

    only consider `Any` delegations

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { dot, dotAh } from "@polkadot-api/descriptors"
-import { createClient } from "polkadot-api"
+import { createClient, Enum } from "polkadot-api"
 import { chainSpec } from "polkadot-api/chains/polkadot"
 import { chainSpec as chainSpecAh } from "polkadot-api/chains/polkadot_asset_hub"
 import { start } from "polkadot-api/smoldot"
@@ -19,9 +19,13 @@ const [relayClient, ahClient] = [relayChain, ahChain].map((chain) =>
 const dotApi = relayClient.getTypedApi(dot)
 const ahApi = ahClient.getTypedApi(dotAh)
 
-const potentialAddresses = (await dotApi.query.Proxy.Proxies.getEntries()).map(
-  ({ keyArgs: [address] }) => address,
-)
+const potentialAddresses = (await dotApi.query.Proxy.Proxies.getEntries())
+  .filter(
+    ({ value: [delegations] }) =>
+      delegations.length === 1 && Enum.is(delegations[0].proxy_type, "Any"),
+  )
+  .map(({ keyArgs: [address] }) => address)
+
 const problematicAddresses = (
   await ahApi.query.System.Account.getValues(potentialAddresses.map((x) => [x]))
 )
